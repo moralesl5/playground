35f1d79a9166e16192ecac480c4194a087c79cb7
moralesl5 <loganjmorales@gmail.com>
2017-09-21 11:13:55 -0400

    About me page started

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 // BUBBLE JS
 var myName = "Logan Morales";
 
@@ -22,6 +24,10 @@ bounceBubbles();
 $(() => {
     $('canvas').width($(window).width())
 
+    // const scrollTop = $(window).scrollTop(),
+    // elementOffset = $('#my-element').offset().top,
+    // distance      = (elementOffset - scrollTop);
+
 
 
     $(window).scroll(function(e) {
@@ -35,6 +41,20 @@ $(() => {
         }
     });
 
+    $(window).scroll(function(e){
+        var $el2 = $('.profile-header')
+        var isPositionFixed = ($el2.css('position') == 'fixed');
+        if ($(this).scrollTop() > 2390 && !isPositionFixed){
+            $el2.css({'position': 'fixed', 'top': '0px'})
+            $('.proj-header').css({ 'display': 'none'});
+        }
+        if ($(this).scrollTop() < 2390 && isPositionFixed){
+            $el2.css({'position': 'static', 'top': '0px'})
+            $('.proj-header').css({ 'position': 'fixed', 'top': '0px' });
+            $('.proj-header').css({ 'display': 'inherit'});
+        }
+    });
+
     $('.project').hover(function() {
         console.log('clicked')
         setTimeout(() => {
@@ -43,10 +63,56 @@ $(() => {
             $('.pText').toggleClass('animated fadeInUp') // WORK HERE!!!
             setTimeout(() =>{
 
-            	$('.button-hide').toggleClass('button-show animated fadeIn')
-            	
+                $('.button-hide').toggleClass('button-show animated fadeIn')
+                
             }, 500)
         }, 100)
     });
 
+    // $(window).scroll(function(e) {
+    //     var $el = $('.profile-header');
+    //     var isPositionFixed = 
+    // })
+
+    // TYPER ------
+    
+    var first_sentence = document.getElementById('first-sentence');
+    var second_sentence = document.getElementById('second-sentence');
+
+    var typewriter = new Typewriter(first_sentence, {
+        typingSpeed: 20
+    });
+
+    var typewriterTwo = new Typewriter(second_sentence,{
+        typingSpeed: 20
+    })
+
+    typewriter.typeString('Logan Morales is a student at Montclair State' 
+                        + ' University and alumni of General Assembly\'s Web' 
+                        + ' Development Immersive program.')
+        .start();
+
+    typewriterTwo.pauseFor(5500)
+        .typeString(' Logan is passionate'
+                    + ' about learning, understanding and the creation of'
+                    + ' online web applications. ')
+        .start();
+
+
+    // MENU ---
+
+    var items = document.querySelectorAll('.circle a');
+
+    for(var i = 0, l = items.length; i < l; i++) {
+      items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+      
+      items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+    }
+
+    document.querySelector('.menu-button').onclick = function(e) {
+       e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+    }
 })
+
+
+
